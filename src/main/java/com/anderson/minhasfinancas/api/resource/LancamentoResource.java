1b72755bdfe33a8618dbbf0c671ea3911e1712bd
Anderson Cardoso Gaspar <anderson.gaspar@unisul.br>
2020-08-25 23:16:28 -0300

    Alterações Finais

--- src/main/java/com/anderson/minhasfinancas/api/resource/LancamentoResource.java
+++ src/main/java/com/anderson/minhasfinancas/api/resource/LancamentoResource.java
@@ -57,6 +57,12 @@ public class LancamentoResource {
 			return ResponseEntity.ok(lancamentos);
 	}
 	
+	@GetMapping("{id}")
+	public ResponseEntity obterLancamento(@PathVariable("id") Long id) {
+		return service.obterPorId(id).map(lancamento -> new ResponseEntity(converter(lancamento), HttpStatus.OK))
+				.orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
+	}
+	
 	@PostMapping
 	public ResponseEntity salvar(@RequestBody LancamentoDTO dto) {
 		try {
@@ -108,6 +114,18 @@ public class LancamentoResource {
 		}).orElseGet(() ->
 			new ResponseEntity("Lancamento não encontrado na base de Dadois.", HttpStatus.BAD_REQUEST));
 	}
+	
+	private LancamentoDTO converter(Lancamento lancamento) {
+		return LancamentoDTO.builder().id(lancamento.getId())
+									  .descricao(lancamento.getDescricao())
+									  .valor(lancamento.getValor())
+									  .mes(lancamento.getMes())
+									  .ano(lancamento.getAno())
+									  .status(lancamento.getStatus().name())
+									  .tipo(lancamento.getTipo().name())
+									  .usuario(lancamento.getUsuario().getId())
+									  .build();
+	}			
 	
 	private Lancamento converter(LancamentoDTO dto) {
 		Lancamento lancamento = new Lancamento();
--- src/main/java/com/anderson/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/anderson/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -98,8 +98,8 @@ public class LancamentoServiceImpl implements LancamentoService{
 //		  BigDecimal big1 = new BigDecimal("0.1");
 //	         BigDecimal big2 = new BigDecimal("0.2");
 		
-		BigDecimal receitas = repository.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.RECEITA);
-		BigDecimal despesas = repository.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.DESPESA);
+		BigDecimal receitas = repository.obterSaldoPorTipoLancamentoEUsuarioEStatus(id, TipoLancamento.RECEITA, StatusLancamento.EFETIVADO);
+		BigDecimal despesas = repository.obterSaldoPorTipoLancamentoEUsuarioEStatus(id, TipoLancamento.DESPESA, StatusLancamento.EFETIVADO);
 		if(receitas == null) {
 			receitas = BigDecimal.ZERO;
 		}
